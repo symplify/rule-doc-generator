d34ee97feec7cb5e137457dd0b5d8f353b956940
Tomas Votruba <tomas.vot@gmail.com>
2023-02-05 10:33:52 +0000

    apply ecs

---
+++ ecs.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+         SetList::SPACES,
+         SetList::ARRAY,
+         SetList::DOCBLOCK,
+         SetList::NAMESPACES,
+         SetList::COMMENTS,
+         SetList::PSR_12,
+    ]);
+};
