5d154b2363dd638f08a5fed0c60c098026802351
Tomas Votruba <tomas.vot@gmail.com>
2023-08-12 17:44:23 +0200

    misc

--- src/FileSystem/PathsHelper.php
+++ src/FileSystem/PathsHelper.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Symplify\RuleDocGenerator\FileSystem;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 final class PathsHelper
 {
     public static function relativeFromDirectory(string $filePath, string $directory): string
     {
-        $relativeFilePath = (string) realpath($filePath);
+        $fileSystem = new Filesystem();
 
-        return str_replace($directory . DIRECTORY_SEPARATOR, '', $relativeFilePath);
+        $relativeFilePath = $fileSystem->makePathRelative(
+            (string) realpath($filePath),
+            (string) realpath($directory)
+        );
+
+        return rtrim($relativeFilePath, '/');
     }
 }
