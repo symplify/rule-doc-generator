b2829ee0b639157df2412a8cb75471afe0ac93b5
GitHub Action <action@github.com>
2024-03-18 20:56:42 +0000

    release PHP 7.2 downgraded

--- bin/rule-doc-generator.php
+++ bin/rule-doc-generator.php
@@ -1,10 +1,10 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace RuleDocGenerator202403;
 
 // 1. autoload
 use Symplify\RuleDocGenerator\DependencyInjection\ContainerFactory;
-
 $possibleAutoloadPaths = [
     // after split package
     __DIR__ . '/../vendor/autoload.php',
@@ -13,24 +13,19 @@ $possibleAutoloadPaths = [
     // monorepo
     __DIR__ . '/../../../vendor/autoload.php',
 ];
-
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
-    if (file_exists($possibleAutoloadPath)) {
+    if (\file_exists($possibleAutoloadPath)) {
         require_once $possibleAutoloadPath;
         break;
     }
 }
-
 // load scoped classes
 $scoperAutoloadFilepath = __DIR__ . '/../vendor/scoper-autoload.php';
-if (file_exists($scoperAutoloadFilepath)) {
+if (\file_exists($scoperAutoloadFilepath)) {
     require_once $scoperAutoloadFilepath;
 }
-
 $containerFactory = new ContainerFactory();
 $container = $containerFactory->create();
-
-
-$application = $container->make(\Symfony\Component\Console\Application::class);
+$application = $container->make(\RuleDocGenerator202403\Symfony\Component\Console\Application::class);
 $exitCode = $application->run();
 exit($exitCode);
---
+++ vendor/illuminate/contracts/Validation/ValidatorAwareRule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RuleDocGenerator202403\Illuminate\Contracts\Validation;
+
+use RuleDocGenerator202403\Illuminate\Validation\Validator;
+interface ValidatorAwareRule
+{
+    /**
+     * Set the current validator.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return $this
+     */
+    public function setValidator(Validator $validator);
+}
