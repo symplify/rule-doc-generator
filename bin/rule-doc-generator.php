b9b0f607201826c181c8315feca07100c2c8a15c
Tomas Votruba <tomas.vot@gmail.com>
2024-03-18 22:14:31 +0100

    load project as well

--- bin/rule-doc-generator.php
+++ bin/rule-doc-generator.php
@@ -23,8 +23,13 @@ foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
 
 // load scoped classes
 $scoperAutoloadFilepath = __DIR__ . '/../vendor/scoper-autoload.php';
-if (file_exists($scoperAutoloadFilepath)) {
+if (\file_exists($scoperAutoloadFilepath)) {
     require_once $scoperAutoloadFilepath;
+
+    // tap into project autoload
+    if (file_exists(__DIR__ . '/../../../autoload.php')) {
+        require_once __DIR__ . '/../../../autoload.php';
+    }
 }
 
 $containerFactory = new ContainerFactory();
